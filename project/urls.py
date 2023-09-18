86b1ceaaa49b972f7d2249c9c8b26549077b8496
IgorPoti <joaooliveira.oliveira134@gmail.com>
2023-09-17 21:00:29 -0300

    Criação de esqueleto

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from recipes.views import home, sobre, contato
+from recipes.views import home
 
 
 urlpatterns = [
--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,4 @@
-from django.http import HttpResponse
-
 from django.shortcuts import render
-
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        
-        'name': 'Igor Cardoso'})
-
-
-def sobre(request):
-    return render(request, 'recipes/temp.html')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'recipes/pages/home.html', context={'name': 'Igor Cardoso'})
